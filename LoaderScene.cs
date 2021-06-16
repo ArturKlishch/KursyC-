d2ca0b30abd6dd0988c72c8807e8dd56f31739d6
ArturKlishch <klischartur@gmail.com>
2021-06-16 23:40:24 +0300

    3
    2

---
+++ LoaderScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderScene : MonoBehaviour
+{
+    public GameController controller;
+
+    private void Awake
+    {
+        if(GameController.intance == null)
+        {
+        Intantiate(controller);
+        }
+        GameController.intance.RespawnPlayer();
+        GameController.intance.ShowGUI();
+
+    }
+
+}
---
+++ teleport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public Vector3 destination;
+    public int mapID;
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadSceneAsenc(mapID);
+        GameController.instance.player.SetPosition(destination);
+    }
+    private IEnumerator.instance.FadeOut()
+        {
+         while(GameController.instance.fadeAnim.IsPlaying)
+        {
+        yield return null;
+        }
+
+    SceneManager.LoadSceneAnync(mapID);
+    GameController.instance.player.SetPosition(destination);
+        }
+  
+
+}
